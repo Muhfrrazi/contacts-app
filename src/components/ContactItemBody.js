a430de3507353ed84a1fc5584950205f15e89812
muhfrrazi <mfachrurrazi41@gmail.com>
2022-11-21 15:25:07 +0800

    menambahkan fungsi tambah data

--- src/components/ContactItemBody.js
+++ src/components/ContactItemBody.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-function ContactItemBody({name, tag}) {
+function ContactItemBody({ name, tag }) {
   return (
     <div className="contact-item__body">
-      <h3>contact-item__title {name}</h3>
+      <h3 className="contact-item__title"> {name}</h3>
       <p className="contact-item__username">@{tag}</p>
     </div>
   );
